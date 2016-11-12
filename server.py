4fdf167327d29758fe61cf446757eb8396b2bd2a
kesshijordan <Kesshi.Jordan@ucsf.edu>
2016-11-12 01:36:17 -0800

    buggy but runs with recluster; add local d3

--- server.py
+++ server.py
@@ -17,20 +17,54 @@ class HelloWorld(object):
     def generate(self):
         result = {"operation": "request", "result": "success"}
 
+        #dmetric = 35
+        #iter = 0
+
         trkpath = '/Users/kjordan/repos/AFQ-viz/client/data/test_ExCap_L.trk'
-        sls = loadtrkfile(trkpath)
-        jsls = makejsontrk(sls)
-        clusters = executeclustering(sls, dist_metric=50)
-        jcsls = makejsonclusters(clusters)
-        #clusters = executeclustering(trkpath, 35, 5)
+        osls = loadtrkfile(trkpath)
+        #ojsls = makejsontrk(osls)
+        oclusters = executeclustering(osls, 35, 5)
         #savejson(clusters, '/Users/kjordan/repos/AFQ-viz/client/data/testingslfcpy.json', 15)
         #makejsonclusters(clusters)
         input_json = cherrypy.request.json  #this is the user information
+        iter = input_json['iteration'] + 1
+        print("IITTTTTEEERRRROOOOOOOOOOOOOO   " + str(iter))
+        dmetric = int(input_json['distance_metric']*0.9)
+        print("NEW DMETRIC "+str(dmetric))
+        tstat = input_json['track_status']
+        #qbcl = input_json['qbclusters']
+
+        '''print("QBCL "+str(qbcl))
+
+        if len(qbcl) < 1:
+            clusters = oclusters
+        else:
+            clusters = qbcl'''
+
+        clusters = oclusters
+
+        sls = []
+        for i,t in enumerate(tstat):
+            if t:
+                print(i)
+                print(t)
+                print "LENCL"
+                print(len(list(clusters[i])))
+                for p,k in enumerate(list(clusters[i])):
+                    sls.append(k)
+        print("LEN SLS "+str(len(sls)))
+        print("Now clustering at "+str(dmetric))
+        clusters = executeclustering(sls, dist_metric=dmetric)
+        print("EXECUTED CL")
+        jcsls = makejsonclusters(clusters)
+        print("Made JSON clusters")
+
         #value = input_json #["my_key"]
         #value["my_key"] = 'this comes from client'
         #value["my_key"] = jsls
-        output_json = jcsls
-
+        print("making output json")
+        output_json = {'clustered_sls' : jcsls, 'iteration': iter, 'distance_metric' : dmetric}
+        print("successful...now returning it")
         #All responses are serialized to JSON. This the same as
         #return simplejson.dumps(result)
         #print("incoming data is", input_json)
@@ -41,5 +75,3 @@ class HelloWorld(object):
 
 if __name__ == '__main__':
     cherrypy.quickstart(HelloWorld(), "/", "Hello.config")
-    #cherrypy.config.update({'server.socket_port': 8090})
-    #cherrypy.engine.restart()
